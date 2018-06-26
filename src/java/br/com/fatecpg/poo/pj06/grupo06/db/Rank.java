8ccb4d486f5c4846daf715e227d77b406ac51ce4
André <Andr@192.168.1.34>
2018-06-26 12:46:36 -0300

    adicionado metodo ranking

---
+++ src/java/br/com/fatecpg/poo/pj06/grupo06/db/Rank.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.fatecpg.poo.pj06.grupo06.db;
+
+import br.com.fatecpg.poo.pj06.grupo06.web.DatabaseConnector;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author André
+ */
+//package br.com.fatecpg.parking.db;
+
+public class Rank {
+    private long id;
+    private String name;
+    private int pontuacao;
+    
+        public Rank(long id, String name, int pontuacao) {
+        this.id = id;
+        this.name = name;
+        this.pontuacao = pontuacao;
+
+    }
+    
+      public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPontuacao() {
+        return pontuacao;
+    }
+
+    public void setPontuacao(int pontuacao) {
+        this.pontuacao = pontuacao;
+    } 
+    
+    public static ArrayList<Rank> getRank()
+            throws Exception{
+        String SQL ="SELECT ID_USUARIO,NOME,PONTUACAO FROM USUARIO";
+        ArrayList<Rank> rank = new ArrayList<>();
+        ArrayList<Object[]> list = DatabaseConnector.executeQuery(SQL, new Object[]{});
+        for(int i=0;i<list.size();i++){
+            Object row[] =  list.get(i);
+            Rank r = new Rank(
+                      (Long)row[0]
+                    , (String) row[1]
+                    , (int)row[2]);
+            rank.add(r);
+        }
+            return rank;
+        
+    }
+
+  
+    
+    
+}
