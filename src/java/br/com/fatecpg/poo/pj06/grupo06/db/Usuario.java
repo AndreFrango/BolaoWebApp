b660b45c315dd2786856dfb9ca3799d661fa642d
Janaina Dias <janainasdias95@gmail.com>
2018-06-27 17:51:35 -0300

    cad usuario ok

--- src/java/br/com/fatecpg/poo/pj06/grupo06/db/Usuario.java
+++ src/java/br/com/fatecpg/poo/pj06/grupo06/db/Usuario.java
@@ -116,17 +116,17 @@ public class Usuario {
     
     
     
-//    public static Usuario validarUsuario(String login, String senha) throws Exception{
-//        String Query = "SELECT * FROM Usuario WHERE login = ? AND senha = ?";
-//        ArrayList<Object[]> list = DatabaseConnector.executeQuery(Query, new Object[]{login, senha.hashCode()});
-//        if(list.size()>0){
-//            Object[] row = list.get(0);
-//            Usuario u = new Usuario((int)row[0], (String)row[1], (Timestamp)row[2], (String)row[3], (int)row[4], (int)row[5], (String)row[6]);
-//            return u;
-//        }else{
-//            return null;
-//        }
-//    }
+    public static Usuario validarUsuario(String login, String senha) throws Exception{
+        String Query = "SELECT * FROM Usuario WHERE login = ? AND senha = ?";
+        ArrayList<Object[]> list = DatabaseConnector.executeQuery(Query, new Object[]{login, senha.hashCode()});
+        if(list.size()>0){
+            Object[] row = list.get(0);
+            Usuario u = new Usuario((int)row[0], (String)row[1], (Timestamp)row[2], (String)row[3], (int)row[4], (int)row[5], (String)row[6]);
+            return u;
+        }else{
+            return null;
+        }
+    }
     
      public static void addUsuario(String nome, Timestamp data, String login, int senha, int pontuacao, String role) throws Exception{
         String SQL = "INSERT INTO USUARIO VALUES("
@@ -136,9 +136,11 @@ public class Usuario {
                 + ",  ?"
                 + ",  ?"
                 + ",  ?"
+                + ",  ?"
                 + ")";
         Object parameters[] = {nome, data, login, senha, pontuacao, role};
         DatabaseConnector.executeCommand(SQL, parameters);
+        
     
     }
     
